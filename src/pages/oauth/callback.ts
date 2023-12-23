a7d3f0ec69c2aea489576019e6ab0c9a5cd2fc0c
SpyTec <eric@spytec.se>
2023-12-23 23:05:22 +0100

    Potentially fix OAuth

---
+++ src/pages/oauth/callback.ts
@@ -0,0 +1,80 @@
+// MIT License
+
+// Copyright (c) 2023 Doruk Gezici
+// https://github.com/dorukgezici/astro-decap-cms-oauth
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import type { APIRoute } from "astro";
+import { clientId, clientSecret, tokenUrl } from "./_config";
+
+export const GET: APIRoute = async ({ url, redirect }) => {
+  const data = {
+    code: url.searchParams.get("code"),
+    client_id: clientId,
+    client_secret: clientSecret,
+  };
+
+  let script;
+
+  try {
+    const response = await fetch(tokenUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const body = await response.json();
+
+    const content = {
+      token: body.access_token,
+      provider: "github",
+    };
+
+    script = `
+      <script>
+        const receiveMessage = (message) => {
+          window.opener.postMessage(
+            'authorization:${content.provider}:success:${JSON.stringify(content)}',
+            message.origin
+          );
+
+          window.removeEventListener("message", receiveMessage, false);
+        }
+        window.addEventListener("message", receiveMessage, false);
+
+        window.opener.postMessage("authorizing:${content.provider}", "*");
+      </script>
+    `;
+
+    return new Response(script, {
+      headers: { "Content-Type": "text/html" },
+    });
+  } catch (err) {
+    console.log(err);
+    return redirect("/?error=Something+went+wrong");
+  }
+};
