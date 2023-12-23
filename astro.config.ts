0a71a0e7a8210c4b487832dfc453ac2ac527e8dd
SpyTec <eric@spytec.se>
2023-12-23 15:46:06 +0100

    Add preline and tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://hemsida-one.vercel.app",
   output: "server",
   adapter: vercel(),
+  integrations: [tailwind()]
 });
